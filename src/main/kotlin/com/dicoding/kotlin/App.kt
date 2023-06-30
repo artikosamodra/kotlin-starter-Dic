cb8e6b208bd4a006b6be66143d76efae0d3f330d
Tao <48190353+Taoisty@users.noreply.github.com>
2023-06-30 20:59:12 +0700

    update app.kt, Data type & Variable (contoh menggunakan operator '+')

--- src/main/kotlin/com/dicoding/kotlin/App.kt
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -1,10 +1,43 @@
 package com.dicoding.kotlin
 
+//Fundamental
 fun main(){
-    val name = "Artiko"
 
+    //Fundamental
+    val name = "Artiko"
 
     print("Hello my name is ")
     println(name)
     println(if (true)"Always true" else "Always false")
+//======================================================================
+
+    //DataTypes & Variable (contoh menggunakan operator '+')
+    val firstWord ="Dicoding "
+    val lastWord = "Academy"
+    print(firstWord+lastWord)
+//======================================================================
+    //Char (contoh menggunakan increment/decrement)
+
+
+
+
+//======================================================================
+
+
+//======================================================================
+
+
+//======================================================================
+
+
+
+//======================================================================
 }
+
+
+
+
+
+
+
+
