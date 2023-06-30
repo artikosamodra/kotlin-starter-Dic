627f4275aed9507f91e10a7b7c50c824992f3819
Tao <48190353+Taoisty@users.noreply.github.com>
2023-06-30 21:26:17 +0700

    update app.kt, String. 1. Escaped String 2. Raw String

--- src/main/kotlin/com/dicoding/kotlin/App.kt
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -16,6 +16,7 @@ fun main(){
     val lastWord = "Academy"
     print(firstWord+lastWord)
 //======================================================================
+
     //Char (contoh menggunakan increment'++'/decrement'--')
     var vocal = 'A'
     println("Vocal "+vocal++) //Increment
@@ -27,7 +28,48 @@ fun main(){
     println("Vocal "+vocal--)
 //======================================================================
 
+    //String
+    val text = "Kotlin"
+    val firstChar = text[0]
 
+    println("First character of $text is $firstChar")
+    for(char in text){
+        print("$char")
+    }
+
+    //Escaped String (Literal String 1)
+    //val statement = "Kotlin is Awesome!" => contoh string
+    //val statement = "Kotlin is "Awesome!"" => penulisan seperti ini akan menyebabkan ambiguitas
+    val statement = "Kotlin is \"Awesome!\"" //=> penulisan dengan cara escaped, dengan tanda \.
+    /* karakter lain
+    \t = add tabs)
+    \n = baris baru
+    \' = ad sinlr quote
+    \" = add double quote
+    \\ = add backslash
+     */
+
+    //Example Escaped String
+    val statement1 = "Kotlin is \"Awesome!\"" // penggunaan \"
+    println("$statement1")
+    val statement2 = "Kotlin is \'Awesome!\'" // penggunaan \'
+    println("$statement2")
+
+    //Raw String (Literal String 2)
+    val line = "Line 1\n"+
+            "Line 2\n"+
+            "Line 3\n"+
+            "Line 4\n"
+    println(line)
+
+    //menjadi
+    val line1 = """
+        Line 1
+        Line 2
+        Line 3
+        Line 4
+    """.trimIndent()
+    println(line1)
 //======================================================================
 
 
